1a3a0edfce538d0f80636d6813d7f96759b28be5
HP laptop <kevintorrezv>
2023-12-08 00:25:11 -0400

    fix new details

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Post } from 'src/app/post.model';
+import { PostService } from 'src/app/post.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  Posts: Post[] //inicializamos un arreglo de tipo Post (que es la clase que importamos). De esta forma se respeta el formato si o si
 
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
   }
